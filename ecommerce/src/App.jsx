95ea9108e91b583b35f8e1a4f736df7a3aa3c0f2
mysilence1408 <mysilence1408@gmail.com>
2025-12-25 14:01:12 +0200

    checkout page completed

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
-      <Route path="checkout" element={<div>hello</div>}></Route>
+      <Route path="checkout" element={<CheckoutPage />}></Route>
     </Routes>
   );
 }
