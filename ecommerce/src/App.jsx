5d4ff11db861d20a3bd17c3674ff9e2739449cec
mysilence1408 <mysilence1408@gmail.com>
2025-12-25 13:23:05 +0200

    react router

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,11 +1,13 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="checkout" element={<div>hello</div>}></Route>
+    </Routes>
   );
 }
 
