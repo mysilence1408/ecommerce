fb9bb8d772200ef0539e92ec1ee872054267f5ff
mysilence1408 <mysilence1408@gmail.com>
2025-12-25 14:26:19 +0200

    tracking and orders pages completed

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import CheckoutPage from "./pages/CheckoutPage";
+import OrdersPage from "./pages/OrdersPage";
+import TrackingPage from "./pages/TrackingPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
       <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route path="orders" element={<OrdersPage />}></Route>
+      <Route path="tracking" element={<TrackingPage />}></Route>
     </Routes>
   );
 }
