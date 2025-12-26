c5c1bd5440abf2c03d2c3df0ee0698476aaa5b49
mysilence1408 <mysilence1408@gmail.com>
2025-12-26 16:35:13 +0200

    lift cart up and shared it between pages

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -4,12 +4,21 @@ import HomePage from "./pages/HomePage";
 import CheckoutPage from "./pages/CheckoutPage";
 import OrdersPage from "./pages/OrdersPage";
 import TrackingPage from "./pages/TrackingPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route path="/" element={<HomePage cart={cart} />}></Route>
+      <Route path="checkout" element={<CheckoutPage cart={cart} />}></Route>
       <Route path="orders" element={<OrdersPage />}></Route>
       <Route path="tracking" element={<TrackingPage />}></Route>
     </Routes>
