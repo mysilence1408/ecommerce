6c7ec45e4f23de64a9462016a27c83351ef53118
mysilence1408 <mysilence1408@gmail.com>
2025-12-26 15:50:57 +0200

    data comes from the backend

--- ecommerce/src/pages/HomePage.jsx
+++ ecommerce/src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./HomePage.css";
 import { Header } from "../components/Header";
-import { products } from "../../starting-code/data/products";
 
 const HomePage = () => {
-  axios.get("http://localhost:3000/api/products").then((response) => {
-    response.data;
-  });
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <>
       <title>Ecommerce Project</title>
